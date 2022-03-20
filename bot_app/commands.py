9250f3ac55389d303bdd2c5a0015bcaccde1f912
SkariOT <vlad.skariot@gmail.com>
2022-03-20 22:17:10 +0100

    +verbs

--- bot_app/commands.py
+++ bot_app/commands.py
@@ -1,46 +1,69 @@
 from aiogram import types
-from aiogram.types import KeyboardButton,ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup
+from aiogram.types import KeyboardButton,ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboardRemove
 from aiogram.dispatcher import FSMContext
 
 from bot_app.states import GameStates
 from .app import dp,bot
 
-
-from get_word_cli import get_and_genirate_words
+import time
+from get_word_cli import get_and_genirate_words,all_verbs,all_words
 
 welcome_msg = "Ceść!\nБот позволяет изучать польские слова и фразы. Список доступных команд:\n/help - возвращет данное сообщение\n/start - возвращет данное сообщение\n/10words - запускает тест на изучение 10 случайных слов"
 
 
-test_btn = KeyboardButton ("/start")
-test_btn2 = KeyboardButton ("/10words")
-rkmu = ReplyKeyboardMarkup(resize_keyboard=True).add(test_btn,test_btn2)
+btn_start = KeyboardButton ("/start")
+btn_10words = KeyboardButton ("/10words")
+btn_10verbs = KeyboardButton ("/10verbs")
+rkmu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_start,btn_10words,btn_10verbs)
+
 
 @dp.message_handler(commands=['start','help'],state="*")
 async def send_welcome(message: types.Message,state: FSMContext):
-    ress = await state.get_state()
-    if 'random' not in str(ress):
+    state_bot = await state.get_state()
+    if 'random' not in str(state_bot):
         await bot.send_message(message.from_user.id,welcome_msg,reply_markup=rkmu)
         await GameStates.start.set()
     else:
-        await message.reply(f"Невозможно повторно запустить новое/повторное задание.\nЗавершите выполнение текущего задания")
-        
+        bad_answer = await message.reply(f"Невозможно повторно запустить новое/повторное задание.\nЗавершите выполнение текущего задания")
+        time.sleep(3)
+        await bad_answer.delete()
+        await message.delete()
+    # await message.delete()
 
-@dp.message_handler(commands=['10words'],state='*')
-async def send_welcome(message: types.Message,state: FSMContext):
-    ress = await state.get_state()
-    if 'random' in str(ress):
-        await message.reply(f"Невозможно повторно запустить новое/повторное задание.\nЗавершите выполнение текущего задания")
+@dp.message_handler(commands=['10words','10verbs'],state='*')
+async def send_start_game(message: types.Message,state: FSMContext):
+    state_bot = await state.get_state()
+    if 'random' in str(state_bot):
+        bad_btn = await message.reply(f"Невозможно повторно запустить новое/повторное задание.\nЗавершите выполнение текущего задания")
+        time.sleep(3)
+        await message.delete()
+        await bad_btn.delete()
         return
-    elif 'None' in str(ress):
+    elif 'None' in str(state_bot):
         await message.reply(f"Сперва выполните команду /start ")
+        await message.delete()
         return
     await GameStates.random_word.set()
     #получая слово, его перевод и варинты 
-    words = get_and_genirate_words()
+    msg_command = str(message.get_command())
+    # data_file_name - переменная с именем файла для загрузки
+    if 'verbs' in msg_command:
+        print("грузим глаголы")
+        data_file_name = 'all_verbs'
+        words = get_and_genirate_words(all_verbs)
+    else:
+        print('Грузим все слова')
+        data_file_name = 'all_words'
+        words = get_and_genirate_words(all_words)
+        
+    fake_word_1=words.get('question_fake')[0]
+    fake_word_2=words.get('question_fake')[1]
+    fake_word_3=words.get('question_fake')[2]
+    
     #наполняю актуальные кнопки информацией
-    inline_button_1 = InlineKeyboardButton(f' {words[2][0]}',callback_data=words[2][0])
-    inline_button_2 = InlineKeyboardButton(f' {words[2][1]}',callback_data=words[2][1])
-    inline_button_3 = InlineKeyboardButton(f' {words[2][2]}',callback_data=words[2][2])
+    inline_button_1 = InlineKeyboardButton(f' {fake_word_1}',callback_data=fake_word_1)
+    inline_button_2 = InlineKeyboardButton(f' {fake_word_2}',callback_data=fake_word_2)
+    inline_button_3 = InlineKeyboardButton(f' {fake_word_3}',callback_data=fake_word_3)
     inline_kb = InlineKeyboardMarkup()
     inline_kb.add(inline_button_1)
     inline_kb.add(inline_button_2)
@@ -49,42 +72,62 @@ async def send_welcome(message: types.Message,state: FSMContext):
     async with state.proxy() as data:
         data['step'] = 1
         data['counter']=0
-        data['answer'] = words[1]
-        data['true_word'] = words[0]
-        data['words'] = [words[2][2],words[2][1],words[2][0]]
-    await message.reply(f" Что означает \'{data['true_word']}\' ?",reply_markup= inline_kb)
+        data['answer_true'] = words.get('answer_true')
+        data['question_true'] = words.get('question_true')
+        data['question_fake'] = words.get('question_fake')
+        data['data_file_name'] = data_file_name
+    msg = await bot.send_message(message.from_user.id,"Приступим к игре!",reply_markup=ReplyKeyboardRemove())
+    time.sleep(2)
+    await msg.delete()
+    await bot.send_message(message.from_user.id,f" Что означает \'{data['question_true']}\' ?",reply_markup = inline_kb)
                           
 
 @dp.callback_query_handler(state=GameStates.random_word)
 async def batton_click_cb(callback_query: types.CallbackQuery,state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
-    answer = callback_query.data
+    answer_cb_data = callback_query.data
     async with state.proxy() as data:
-        curr_word = data.get("true_word")
-        curr_ans = data.get('answer')
-        words = get_and_genirate_words()
-        inline_button_1 = InlineKeyboardButton(f' {words[2][0]}',callback_data=words[2][0])
-        inline_button_2 = InlineKeyboardButton(f' {words[2][1]}',callback_data=words[2][1])
-        inline_button_3 = InlineKeyboardButton(f' {words[2][2]}',callback_data=words[2][2])
+        current_quiestion_true = data.get("question_true")
+        current_answer_true = data.get('answer_true')
+        file_name = data.get('data_file_name')
+        #из даты,получаю имя файла который требуется "найти"
+        #через глобалс, по имени получаю саму переменную
+        file_load_name = globals()[file_name]
+        if 'verb' in file_name:
+            words = get_and_genirate_words(file_load_name)
+        else:
+            words = get_and_genirate_words(file_load_name)
+        fake_word_1=words.get('question_fake')[0]
+        fake_word_2=words.get('question_fake')[1]
+        fake_word_3=words.get('question_fake')[2]
+        inline_button_1 = InlineKeyboardButton(f' {fake_word_1}',callback_data=fake_word_1)
+        inline_button_2 = InlineKeyboardButton(f' {fake_word_2}',callback_data=fake_word_2)
+        inline_button_3 = InlineKeyboardButton(f' {fake_word_3}',callback_data=fake_word_3)
         inline_kb = InlineKeyboardMarkup()
         inline_kb.add(inline_button_1)
         inline_kb.add(inline_button_2)
         inline_kb.add(inline_button_3)
         
 
-        data['answer'] = words[1]
-        data['true_word'] = words[0]
-        if answer == curr_ans:
+        data['answer_true'] = words.get('answer_true')
+        data['question_true'] = words.get('question_true')
+        if answer_cb_data == current_answer_true:
             data['counter'] += 1
-            await bot.send_message(callback_query.from_user.id,f" Очень хорошо!  \'{curr_word}\' это \'{answer}\'")
+            msg = await bot.send_message(callback_query.from_user.id,f" Очень хорошо!  \'{current_quiestion_true}\' это \'{answer_cb_data}\'")
         else:
-            await bot.send_message(callback_query.from_user.id,f"Ваш варинт ответа \'{answer}\' не верен.\n Верное значение \'{curr_ans}\'")
+            msg = await bot.send_message(callback_query.from_user.id,f"Ваш варинт ответа \'{answer_cb_data}\' не верен.\n Верное значение \'{current_answer_true}\'")
+        
+        # await callback_query.message.delete()
+        # time.sleep(3)
+        # await msg.delete()
         
         if data['step'] >= 10:
-            await bot.send_message(callback_query.from_user.id,f"Игра окончина.\nКол-во верных ответов {data['counter']} из {data['step']} попыток")
+            await bot.send_message(callback_query.from_user.id,f"Игра окончина!\nКол-во верных ответов {data['counter']} из {data['step']} попыток",reply_markup=rkmu)
             await GameStates.start.set()
             return 
         else:
-            await bot.send_message(callback_query.from_user.id,f"Кол-во верных ответов {data['counter']} из {data['step']} попыток")
-            await bot.send_message(callback_query.from_user.id,f" Что означает \'{data['true_word']}\' ?",reply_markup = inline_kb)
+            msg = await bot.send_message(callback_query.from_user.id,f"Кол-во верных ответов {data['counter']} из {data['step']} попыток")
+            # time.sleep(3)
+            # await msg.delete()
+            await bot.send_message(callback_query.from_user.id,f"Что означает \'{data['question_true']}\' ?",reply_markup = inline_kb)
             data['step'] += 1
--- get_word_cli.py
+++ get_word_cli.py
@@ -1,48 +1,59 @@
 import random
 
 
-def get_and_genirate_words(): 
-    # file_name = '10_sloynik.txt'
-    file_name = 'all_words.txt'
-    slowy = {}
+file_name_all_words = 'all_words.txt'
+file_name_all_verbs = 'verbs.txt'
+
+
+def load_words(file_name):
+    words_all = {}
     with open(file_name,'r',encoding='utf-8-sig') as read_sloy:
         for line in read_sloy.readlines():
             split_line = line.replace(" – "," - ").replace("\n","").replace("\ufeff",'').split(" - ")
-            slowy[split_line[0]]=split_line[1]
+            words_all[split_line[0]]=split_line[1]
+    return words_all
 
-    res_key = random.choice(list(slowy.keys()))
-    res_val_true = slowy.get(res_key)
+all_words = load_words(file_name_all_words)
+all_verbs = load_words(file_name_all_verbs)
+
+def get_and_genirate_words(words): 
+
+    quiestion_key = random.choice(list(words.keys()))
+    answer_true = words.get(quiestion_key)
 
     #добиваюст рандомное слово из словоря/базы
-    res_val = random.choice(list(slowy.values()))
-    while res_val == slowy.get(res_key):
-        res_val = random.choice(list(slowy.values()))
-    res_val_2 = random.choice(list(slowy.values()))
-    while (res_val_2 == slowy.get(res_key)) or res_val_2==res_val:
-        res_val_2 = random.choice(list(slowy.values()))
+    answer_fake_1 = random.choice(list(words.values()))
+    while answer_fake_1 == words.get(quiestion_key):
+        answer_fake_1 = random.choice(list(words.values()))
+    answer_fake_2 = random.choice(list(words.values()))
+    while answer_fake_2 == answer_true or answer_fake_2==answer_fake_1:
+        answer_fake_2 = random.choice(list(words.values()))
 
     #верное слово + 2 рамдомных закиываю в один лист    
-    res_list = [res_val_true,res_val,res_val_2]
+    res_list = [answer_true,answer_fake_1,answer_fake_2]
 
     # "перемешиваю эти 3 слова"
-    rand_pos_list = random.sample(res_list,3)
-    return res_key,res_val_true,rand_pos_list
+    rand_quiestion_list = random.sample(res_list,3)
+    resault = {}
+    resault = {'question_true' : quiestion_key,'answer_true':answer_true,'question_fake': rand_quiestion_list}
+    return resault
+    # return quiestion_key,answer_true,rand_quiestion_list
 
     #тестовый блок, для работы в консоли 
     
     
-    # print("_"*20+'\n',f"Слово {res_key}")
+    # print("_"*20+'\n',f"Слово {quiestion_key}")
     # #при выводе вывожу по порядку "перемешенный" список
     # print(f"1){rand_pos_list[0]}")
     # print(f"2){rand_pos_list[1]}")
     # print(f"3){rand_pos_list[2]}")
     # inp = input("Выберите номер верного значения: ")
 
-    # if rand_pos_list[int(inp)-1] == res_val_true:
-    #     print(f"Всё верно {res_key} это {res_val_true}")
+    # if rand_pos_list[int(inp)-1] == answer_true:
+    #     print(f"Всё верно {quiestion_key} это {answer_true}")
     # else:
-    #     print(f"{rand_pos_list[int(inp)-1]} не является переводом для слова {res_key}")
-    #     print(f"Верный перевод слова {res_key} - {res_val_true}")
+    #     print(f"{rand_pos_list[int(inp)-1]} не является переводом для слова {quiestion_key}")
+    #     print(f"Верный перевод слова {quiestion_key} - {answer_true}")
         
 # res = get_and_genirate_words()
-# print(res)
+# print(res.get('question_fake')[0])
